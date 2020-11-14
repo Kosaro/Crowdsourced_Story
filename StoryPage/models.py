92d5f6d03ad70f4061a4dba13b1384ed93e378ae
sean-cherry <sac17@my.fsu.edu>
2020-11-14 14:14:55 -0500

    database schema created in accordance to stage 3 submission. the only thing missing is the 'destination' attribute for the choice class.

--- StoryPage/models.py
+++ StoryPage/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+class PlotPoint(models.Model):
+    pptext = models.CharField(max_length=1000)
+    uv = models.IntegerField(default=0)
+    dv = models.IntegerField(default=0)
+    isEnd = models.BooleanField(default=False)
+    writtenby = models.CharField(max_length=50)
+
+class Choice(models.Model):
+    plotpoint = models.ForeignKey(PlotPoint, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+    uv = models.IntegerField(default=0)
+    dv = models.IntegerField(default=0)
+    writtenby = models.CharField(max_length=50)
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=30)
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plot_point = models.ForeignKey(PlotPoint, on_delete=models.CASCADE)
+
+class Edits(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plot_point = models.ForeignKey(PlotPoint, on_delete=models.CASCADE)
+    request_status = models.BooleanField(default=True)
+
+class Choice_Edits(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plot_point = models.ForeignKey(PlotPoint, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    request_status = models.BooleanField(default=True)
+
+class Upvotes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plot_point = models.ForeignKey(PlotPoint, on_delete=models.CASCADE)
+
+class Downvotes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plot_point = models.ForeignKey(PlotPoint, on_delete=models.CASCADE)
+
+class Choice_Upvotes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plot_point = models.ForeignKey(PlotPoint, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+class Choice_Downvotes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plot_point = models.ForeignKey(PlotPoint, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)

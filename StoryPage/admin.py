289000e58c302ccc03766b4d45fc92289505e64e
Timur726 <72626447+Timur726@users.noreply.github.com>
2020-11-29 19:14:52 -0500

    Update admin.py
    Model registrations for Admin UI in admin.py

--- StoryPage/admin.py
+++ StoryPage/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from Crowdsourced_Story.models import PlotPoint, Choice, User
 
-# Register your models here.
+
+@admin.register(PlotPoint)
+class PlotPointAdmin(admin.ModelAdmin):
+    list_display = ['pptext', 'writtenby', 'uv', 'dv']
+    search_fields = ['pptext__startswith']
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['text', 'writtenby', 'uv', 'dv']
+    search_fields = ['text__startswith']
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username']
+    search_fields = ['username__startswith']

ae45b425bc1c25e9769b6bfa53abb655be36a60a
sean-cherry <sac17@my.fsu.edu>
2020-12-01 21:33:00 -0500

    api functions added in views.py

--- StoryPage/views.py
+++ StoryPage/views.py
@@ -1,19 +1,63 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from StoryPage.models import PlotPoint, Choice, Upvotes, Choice_Upvotes, Downvotes, Choice_Downvotes, Bookmark
 
 
-def plot_point(request):
-    text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' \
-           'sed do eiusmod tempor incididunt ut labore et dolore magna ' \
-           'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ' \
-           'ullamco laboris nisi ut aliquip ex ea commodo consequat. ' \
-           'Duis aute irure dolor in reprehenderit in voluptate velit esse ' \
-           'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat ' \
-           'cupidatat non proident, sunt in culpa qui officia deserunt mollit ' \
-           'anim id est laborum'
-    choices = ['Choice 1', 'Choice 2', 'Choice 3']
-    author = 'Ella'
-    votes = 0
+# this is the same render function that was filled with dummy data, but you have to pass a PlotPoint object to it.
+def plot_point(request, plot_point):
+    text = plot_point.pptext
+    choices = get_choices(plot_point)
+    author = plot_point.writtenby
+    votes = plot_point.uv
     context = {'plot_point': text, 'choices': choices, 'votes': votes, 'author': author, 'username': 'Jenna the Rogue'}
     return render(request, 'plotPoint.html', context)
 
+
+# returns a python dictionary of ALL of the choices for the passed-in plot point, ordered by upvote count.
+def get_choices(plot_point):
+    choicelist = Choice.objects.all().filter(plotpoint=plot_point).order_by('uv').values('text')
+    return choicelist
+
+
+# creates a new choice, and the following plot point. auto-upvotes for author. returns the new plotpoint object.
+def add_choice(request, prevpp, choice_text, plotpoint_text, author):
+    prevpp.isEnd = False
+    prevpp.save()
+    newplotpoint = PlotPoint(pptext=plotpoint_text, writtenby=author)
+    newplotpoint.save()
+    newchoice = Choice(text = choice_text, writtenby= author, plotpoint = prevpp, destination=newplotpoint.id)
+    newchoice.save()
+    upvote(newplotpoint,author)
+    return newplotpoint
+
+
+# adds an upvote between the plot point and the user
+def upvote(request, plot_point, current_user):
+    plot_point.uv = plot_point.uv + 1
+    newUV = Upvotes(user=current_user, plot_point=plot_point)
+    newUV.save()
+
+
+# same thing but for choices
+def choice_upvote(request, choice, current_user):
+    choice.uv = choice.uv + 1
+    newUV = Choice_Upvotes(user=current_user, choice=choice)
+    newUV.save()
+
+
+# ditto
+def downvote(request, plot_point, current_user):
+    plot_point.dv = plot_point.dv - 1
+    newDV = Downvotes(user=current_user, plot_point=plot_point)
+    newDV.save()
+
+
+def choice_downvote(request, choice, current_user):
+    choice.dv = choice.dv + 1
+    newDV = Choice_Downvotes(user=current_user, choice=choice)
+    newDV.save()
+
+
+# favorite but i called it bookmark in the db
+def bookmark(request, plot_point, current_user):
+    newbookmark = Bookmark(user=current_user, plot_point=plot_point)
+    newbookmark.save()
